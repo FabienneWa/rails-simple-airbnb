b379c5a4106f15fb3bbf80492ef4ccf9466da917
Fabienne Walch <fabienne_walch@hotmail.com>
2020-10-21 22:01:34 +0200

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+flat1 = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+flat2 = Flat.create!(
+  name: 'Light Flat London',
+  address: 'Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 2
+)
+
+flat3 = Flat.create!(
+  name: 'Spacious Garden Flat London',
+  address: 'Vienna',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 45,
+  number_of_guests: 8
+)
+
+flat4 = Flat.create!(
+  name: 'good one',
+  address: 'Amsterdam',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 105,
+  number_of_guests: 2
+)
+
+flat5 = Flat.create!(
+  name: 'nice room',
+  address: 'Milan',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 150,
+  number_of_guests: 3
+)
